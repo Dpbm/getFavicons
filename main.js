1f0c0df00340a5832b011e5013e4e218696dfe23
Dpbm <dpbm136@gmail.com>
2022-09-21 13:37:19 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const button = document.getElementById('getFavicons');
+const list = document.getElementById('faviconsList');
+
+function createChild(link, title) {
+  const aElement = document.createElement('a');
+  aElement.setAttribute('href', link);
+  aElement.setAttribute('target', '_blank');
+  aElement.setAttribute('download', title);
+  aElement.setAttribute('title', 'download');
+
+  const imgElement = document.createElement('img');
+  imgElement.setAttribute('src', link);
+
+  aElement.appendChild(imgElement);
+
+  list.appendChild(aElement);
+
+}
+
+async function getFavicons() {
+  browser.tabs.query({ currentWindow: true }).then((tabs) => {
+    list.innerHTML = "";
+
+    for (const tab of tabs) {
+      const { favIconUrl, title } = tab;
+
+      if (favIconUrl) createChild(favIconUrl, title);
+    }
+  });
+}
+
+
+button.addEventListener('click', getFavicons);
+
+
+
